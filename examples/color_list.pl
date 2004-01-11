36ab5d7ffd709e1552f9f82bafab3a488cb5d783
Scott Arrington <sarringt@src.gnome.org>
2004-01-11 05:58:37 +0000

    commentary and cleanup
    svn path=/trunk/; revision=921

--- examples/color_list.pl
+++ examples/color_list.pl
@@ -1,12 +1,25 @@
 #!/usr/bin/perl
 
+=doc
+
+This example shows one way to colorize rows in a SimpleList.
+
+The basic approach is to add a color attribute to the columns in the TreeView
+(remember that a SimpleList is a TreeView), and store the color information in
+a hidden column in the model.  Since the Glib type system will be used to fetch
+the color attribute, the hidden column must be of type Gtk2::Gdk::Color, which
+requires us to add a custom column type to SimpleList.
+
+=cut
+
 use strict;
 use warnings;
-
+use constant TRUE  => 1;
+use constant FALSE => 0;
 use Gtk2 '-init';
 use Gtk2::SimpleList;
 
-# add a new type of column that reverses the text that's in a scalar
+# add a new hidden column that holds a Gtk2::Gdk::Color.
 Gtk2::SimpleList->add_column_type(
 	'color',
 		type     => 'Gtk2::Gdk::Color',
@@ -15,7 +28,6 @@ Gtk2::SimpleList->add_column_type(
 	);
 
 
-# create a simple list widget with one of each column type
 my $slist = Gtk2::SimpleList->new (
 			'Text Field'    => 'text',
 			'Int Field'     => 'int',
@@ -23,38 +35,40 @@ my $slist = Gtk2::SimpleList->new (
 			'row color'     => 'color',
 	);
 
-my $red = Gtk2::Gdk::Color->new (0xFFFF, 0, 0);
-my $grn = Gtk2::Gdk::Color->new (0, 0xFFFF, 0);
+#
+# add a color attribute to each column, getting the color information
+# from the hidden column.
+#
+foreach my $col ($slist->get_columns) {
+	foreach my $cell ($col->get_cell_renderers) {
+		$col->add_attribute ($cell, cell_background_gdk => 3);
+	}
+}
 
+#
+# now put some data into the list; note the data for the hidden column.
+# we'll leave a couple of rows uncolored.
+#
+@{$slist->{data}} = (
+	[ 'Red',     1, FALSE, Gtk2::Gdk::Color->new (0xFFFF, 0, 0)      ],
+	[ 'Green',   2, TRUE,  Gtk2::Gdk::Color->new (0, 0xFFFF, 0)      ],
+	[ 'Blue',    3, FALSE, Gtk2::Gdk::Color->new (0, 0, 0xFFFF)      ],
+	[ 'Cyan',    4, TRUE,  Gtk2::Gdk::Color->new (0, 0xFFFF, 0xFFFF) ],
+	[ 'Magenta', 5, FALSE, Gtk2::Gdk::Color->new (0xFFFF, 0, 0xFFFF) ],
+	[ 'Yellow',  6, TRUE,  Gtk2::Gdk::Color->new (0xFFFF, 0xFFFF, 0) ],
+	[ 'plain',   7, FALSE, undef ],
+	[ 'plain',   8, TRUE,  undef ],
+);
+
+
+
+# the rest is uninteresting.
 my $win = Gtk2::Window->new;
 $win->set_title ('Colorizing A List');
 $win->set_border_width (6);
-$win->set_default_size (300, 250);
 $win->signal_connect (delete_event => sub { Gtk2->main_quit; });
 
-my $vbox = Gtk2::VBox->new (0, 6);
-$win->add ($vbox);
-
-my $scwin = Gtk2::ScrolledWindow->new;
-$vbox->pack_start ($scwin, 1, 1, 0);
-$scwin->add ($slist);
-
-@{$slist->{data}} = (
-	[ 'One', 1, 0, $red ],
-	[ 'Two', 2, 1, $grn ],
-	[ 'Three', 3, 0, $red ],
-	[ 'Four', 4, 1, $grn ],
-	[ 'Five', 5, 0, $red ],
-	[ 'Six', 6, 1, $grn ],
-	[ 'Seven', 7, 0, $red ],
-	[ 'Eight', 8, 1, $grn ],
-);
-
-for(my $i = 0; $i < $slist->get_model->get_n_columns-1; $i++)
-{
-	my $col = $slist->get_column($i);
-	$col->add_attribute($col->get_cell_renderers, cell_background_gdk => 3);
-}
+$win->add ($slist);
 
 $win->show_all;
 Gtk2->main;
