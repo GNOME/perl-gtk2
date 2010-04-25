91e41f92ca965735ea6be0ebcfa4c241ab3321b0
Torsten Schönfeld <kaffeetisch@gmx.de>
2010-04-25 18:24:36 +0200

    Release 1.230

--- Gtk2.pm
+++ Gtk2.pm
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2003-2008 by the gtk2-perl team (see the file AUTHORS for
+# Copyright (C) 2003-2010 by the gtk2-perl team (see the file AUTHORS for
 # the full list)
 #
 # This library is free software; you can redistribute it and/or modify it under
@@ -73,7 +73,7 @@ eval "use Cairo;";
 use Exporter;
 require DynaLoader;
 
-our $VERSION = '1.220';
+our $VERSION = '1.230';
 
 our @ISA = qw(DynaLoader Exporter);
 
@@ -405,7 +405,7 @@ The gtk2-perl team:
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2003-2008 by the gtk2-perl team.
+Copyright 2003-2010 by the gtk2-perl team.
 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Library General Public
